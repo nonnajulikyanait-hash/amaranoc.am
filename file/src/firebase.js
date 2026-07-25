29f0ac2ffe3325b63aabdc4b2ed64006b7fa8cff
Nonna <nonna@MacBook-Air-Nonna.local>
2026-07-25 16:54:48 +0400

    firebase

--- file/src/firebase.js
+++ file/src/firebase.js
@@ -1,23 +1,38 @@
+// // Import the functions you need from the SDKs you need
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+// // TODO: Add SDKs for Firebase products that you want to use
+// // https://firebase.google.com/docs/web/setup#available-libraries
+
+// // Your web app's Firebase configuration
+// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+// const firebaseConfig = {
+//   apiKey: "AIzaSyA5Av-xVDrDWRXqJdpWVT1LMSpx3m2NPYQ",
+//   authDomain: "aranoc-am.firebaseapp.com",
+//   projectId: "aranoc-am",
+//   storageBucket: "aranoc-am.firebasestorage.app",
+//   messagingSenderId: "453033838051",
+//   appId: "1:453033838051:web:401b8f109f26ba3fac4cd6",
+//   measurementId: "G-51P0PVGBRY"
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore"; // Ավելացրու սա
-import { getAuth } from "firebase/auth"; // Ավելացրու սա
+import { getDatabase } from "firebase/database";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCLkH4KAZwt0485pagnAYQLeDzZ3IPWSsg",
-  authDomain: "chat-64147.firebaseapp.com",
-  projectId: "chat-64147",
-  storageBucket: "chat-64147.firebasestorage.app",
-  messagingSenderId: "1007509998547",
-  appId: "1:12087134492:web:e8c5589a23d9b306c692d9", // Նկատիր՝ այստեղ քո նախորդի appId-ն էր, համոզվիր որ սա ճիշտ է
-  measurementId: "G-7SY8VQNRGP"
+  apiKey: "AIzaSyA5Av-xVDrDWRXqJdpWVT1LMspx3m2NPYQ",
+  authDomain: "aranoc-am.firebaseapp.com",
+  databaseURL: "https://aranoc-am-default-rtdb.firebaseio.com", // Սա պետք է ավելացնեք, որպեսզի բազան աշխատի
+  projectId: "aranoc-am",
+  storageBucket: "aranoc-am.appspot.com",
+  messagingSenderId: "453033838051",
+  appId: "1:453033838051:web:401b8f109f26ba3fac4cd6",
+  measurementId: "G-51P0PVGBRY"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
-// Այս երկուսը պարտադիր են
-export const db = getFirestore(app);
-export const auth = getAuth(app);
+export const db = getDatabase(app);
