62ffdb2acba7695fbd030bf12a90c76ef9d4d8d9
Nonna <nonna@MacBook-Air-Nonna.local>
2026-07-29 20:11:05 +0400

    changings

--- file/src/UseFavourites.js
+++ file/src/UseFavourites.js
@@ -1,11 +1,17 @@
 import { create } from 'zustand';
 
-const useFavoritesStore = create((set) => ({
-  favoritesCount: 0,
-  
-  increment: () => set((state) => ({ favoritesCount: state.favoritesCount + 1 })),
-  
-  decrement: () => set((state) => ({ favoritesCount: Math.max(0, state.favoritesCount - 1) })),
+const useFavouritesStore = create((set) => ({
+  favourites: [],
+
+  toggleFavourite: (item) =>
+    set((state) => {
+      const exists = state.favourites.some((fav) => fav.id === item.id);
+      if (exists) {
+        return { favourites: state.favourites.filter((fav) => fav.id !== item.id) };
+      } else {
+        return { favourites: [...state.favourites, item] };
+      }
+    }),
 }));
 
-export default useFavoritesStore;
+export default useFavouritesStore;
