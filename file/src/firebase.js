5f406805d6fd992ed24b2951e26df9c495bde994
Nonna <nonna@MacBook-Air-Nonna.local>
2026-08-19 18:51:09 +0400

    update firebase config

--- file/src/firebase.js
+++ file/src/firebase.js
@@ -1,25 +1,3 @@
-// // Import the functions you need from the SDKs you need
-// import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-// // TODO: Add SDKs for Firebase products that you want to use
-// // https://firebase.google.com/docs/web/setup#available-libraries
-
-// // Your web app's Firebase configuration
-// // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-// const firebaseConfig = {
-//   apiKey: "AIzaSyA5Av-xVDrDWRXqJdpWVT1LMSpx3m2NPYQ",
-//   authDomain: "aranoc-am.firebaseapp.com",
-//   projectId: "aranoc-am",
-//   storageBucket: "aranoc-am.firebasestorage.app",
-//   messagingSenderId: "453033838051",
-//   appId: "1:453033838051:web:401b8f109f26ba3fac4cd6",
-//   measurementId: "G-51P0PVGBRY"
-// };
-
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-
 import { initializeApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAuth } from "firebase/auth";
