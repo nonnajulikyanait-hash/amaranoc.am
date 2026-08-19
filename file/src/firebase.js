181cdd9413a2f1597c833fd6e826301128444152
Nonna <nonna@MacBook-Air-Nonna.local>
2026-08-19 19:15:52 +0400

    fix firebase config with analytics db and auth

--- file/src/firebase.js
+++ file/src/firebase.js
@@ -1,13 +1,12 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyA5Av-xVDrDWRXqJdpWVT1LMSpx3m2NPYQ",
+  apiKey: "AIzaSyA5Av-xVDrDWRXqJdpWVT1LMspx3m2NPYQ",
   authDomain: "aranoc-am.firebaseapp.com",
   databaseURL: "https://aranoc-am-default-rtdb.firebaseio.com",
   projectId: "aranoc-am",
@@ -20,3 +19,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+
+// Export db and auth for your components
+export const db = getDatabase(app);
+export const auth = getAuth(app);
